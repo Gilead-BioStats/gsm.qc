f9b36c9f89cbb2b4e9237081e46be50730e71335
Laura Maxwell <laura.maxwell@atorusresearch.com>
2025-06-05 11:07:40 -0400

    fix qual tests to align with gsm.kri 1.1.2 changes

--- tests/testqualification/qualification/test-qual_T13_2.R
+++ tests/testqualification/qualification/test-qual_T13_2.R
@@ -5,7 +5,7 @@ kri_custom <- MakeWorkflowList(c(sprintf("kri%04d_custom", 8:9), sprintf("cou%04
 ## Test Code
 testthat::test_that("Query Rate Assessments can be done correctly using a grouping variable, such as Site, Country, or Study, when applicable.", {
   ## regular -----------------------------------------
-  test <- map(kri_workflows, ~ robust_runworkflow(.x, mapped_data, steps = 1:5))
+  test <- map(kri_workflows, ~ robust_runworkflow(.x, mapped_data, steps = 1:6))
 
   # grouping col in yaml file is interpreted correctly in dfInput GroupID
   iwalk(test, ~ expect_identical(
@@ -13,11 +13,25 @@ testthat::test_that("Query Rate Assessments can be done correctly using a groupi
     sort(unique(.x$Mapped_SUBJ[[kri_workflows[[.y]]$steps[[which(map_chr(kri_workflows[[.y]]$steps, ~ .x$name) == "gsm.core::Input_Rate")]]$params$strGroupCol]])) # No guarantee the Input_Rate mapping is done step 2, need better index
   ))
 
+  test_new <- test[-3]
+
   # data is properly transformed by correct group in dfTransformed
-  iwalk(test, ~ expect_equal(
-    n_distinct(.x$Mapped_SUBJ[[kri_workflows[[.y]]$steps[[which(map_chr(kri_workflows[[.y]]$steps, ~ .x$name) == "gsm.core::Input_Rate")]]$params$strGroupCol]]),
-    nrow(.x$Analysis_Transformed)
-  ))
+  expect_equal(
+    n_distinct(test$cou0008$Mapped_SUBJ[[kri_workflows[[1]]$steps[[which(map_chr(kri_workflows[[1]]$steps, ~ .x$name) == "gsm.core::Input_Rate")]]$params$strGroupCol]]),
+    nrow(test$cou0008$Analysis_Transformed)
+  )
+  expect_equal(
+    n_distinct(test$cou0009$Mapped_SUBJ[[kri_workflows[[2]]$steps[[which(map_chr(kri_workflows[[2]]$steps, ~ .x$name) == "gsm.core::Input_Rate")]]$params$strGroupCol]]),
+    nrow(test$cou0009$Analysis_Transformed)
+  )
+  expect_equal(
+    n_distinct(test$kri0008$Mapped_SUBJ[[kri_workflows[[3]]$steps[[which(map_chr(kri_workflows[[3]]$steps, ~ .x$name) == "gsm.core::Input_Rate")]]$params$strGroupCol]])-7,
+    nrow(test$kri0008$Analysis_Transformed)
+  )
+  expect_equal(
+    n_distinct(test$kri0009$Mapped_SUBJ[[kri_workflows[[4]]$steps[[which(map_chr(kri_workflows[[4]]$steps, ~ .x$name) == "gsm.core::Input_Rate")]]$params$strGroupCol]])-7,
+    nrow(test$kri0009$Analysis_Transformed)
+  )
 
   ## custom -------------------------------------------
   test_custom <- map(kri_custom, ~ robust_runworkflow(.x, mapped_data, steps = 1:5))
--- tests/testqualification/qualification/test-qual_T8_2.R
+++ tests/testqualification/qualification/test-qual_T8_2.R
@@ -5,7 +5,7 @@ kri_custom <- MakeWorkflowList(c("kri0006_custom", "kri0007_custom", "cou0006_cu
 ## Test Code
 testthat::test_that("Disposition Assessments can be done correctly using a grouping variable, such as Site or Country for KRIs, and Study for QTLs, when applicable.", {
   ## regular -----------------------------------------
-  test <- map(kri_workflows, ~ robust_runworkflow(.x, mapped_data, steps = 1:4))
+  test <- map(kri_workflows, ~ robust_runworkflow(.x, mapped_data, steps = 1:5))
 
   # grouping col in yaml file is interpreted correctly in dfInput GroupID
   iwalk(test, ~ expect_identical(
