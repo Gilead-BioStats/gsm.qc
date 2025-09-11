911002ac8077a56f26834e0510592619534cef5a
zdz2101 <zelos.zhu@atorusresearch.com>
2025-09-11 10:12:02 -0700

    cleanup

--- tests/testqualification/qualification/test-qual_T16_1.R
+++ tests/testqualification/qualification/test-qual_T16_1.R
@@ -1,41 +1,52 @@
 ## Test Setup
-kri_workflows <- MakeWorkflowList(strNames = "kri", strPath = "workflow/2_metrics", strPackage = "gsm.kri")
-analyzed <- RunWorkflows(kri_workflows[-13], mapped_data) # Exclude pk/pd since thats not counting to SRS
-
-reporting_workflows <- MakeWorkflowList(strPackage = "gsm.reporting")
-historical_reporting_results <- gsm.core::reportingResults %>% dplyr::filter(SnapshotDate < max(.data$SnapshotDate))
-reporting <- RunWorkflows(reporting_workflows, lData = c(mapped_data, list(lAnalyzed = analyzed,
-                                                                        lWorkflows = kri_workflows[-13])))
+kri_workflows <- MakeWorkflowList(strNames = c(paste0("kri000", 1:9), paste0("kri00", 10:12), "srs"), strPath = "workflow/2_metrics", strPackage = "gsm.kri")
+analyzed <- RunWorkflows(kri_workflows, mapped_data) # Exclude pk/pd since thats not counting to SRS
 
 ## Test Code
 testthat::test_that("Given summarized analytics data, all appropriate aspects of site risk score are available to calculate it correctly", {
-  # Check all kri workflows have 1:1 mapped flags and respective weights
+  # Check all kri workflows have 1:1 mapped flags and respective weights, exclude PK-PD and SRS
   expect_equal(
     map(kri_workflows[-13], function(x) length(strsplit(x$meta$Flag, ",")[[1]])) ,
     map(kri_workflows[-13], function(x) length(strsplit(x$meta$RiskScoreWeight, ",")[[1]]))
   )
 
   # Check that all Analysis_Flagged data frames contain columns for Weight and WeightMax
-  expect_true(all(unlist(map(analyzed, function(x) all(c("Weight", "WeightMax") %in% names(x$Analysis_Flagged))))))
+  expect_true(all(unlist(map(analyzed[-13], function(x) all(c("Weight", "WeightMax") %in% names(x$Analysis_Flagged))))))
 
   # Check Site Risk Score matches by hand vs using gsm.kri functions
-  SRS_by_hand <- map2(
-    analyzed,
-    names(analyzed),
+  global_weight <- map2(
+    analyzed[-13],
+    names(analyzed)[-13],
     function(x, y) {
-      x$Analysis_Summary %>%
-        mutate(MetricID = y) %>%
-        inner_join(., gsm.kri::metricWeights, by = c("MetricID", "Flag"))
+      x$Analysis_Flagged %>%
+        mutate(MetricID = y)
+    }
+  ) %>%
+    bind_rows() %>%
+    summarize(GlobalDenominator = sum(max(WeightMax), na.rm = TRUE), .by = MetricID) %>%
+    pull(GlobalDenominator) %>%
+    sum
+
+  SRS_by_hand <- map2(
+    analyzed[-13],
+    names(analyzed)[-13],
+    function(x, y) {
+      x$Analysis_Flagged %>%
+        mutate(MetricID = y)
     }
   ) %>%
     bind_rows() %>%
     group_by(GroupID) %>%
     summarize(Numerator = sum(Weight, na.rm = TRUE),
-              Denominator = sum(WeightMax, na.rm = TRUE),
+              Denominator = global_weight,
               Metric = Numerator/Denominator * 100,
-              Score = Metric)
+              Score = Metric) %>%
+    ungroup() %>%
+    mutate(GroupLevel = "Site",
+           MetricID = "Analysis_srs0001",
+           Flag = NA) %>%
+    select(GroupLevel, GroupID, MetricID, Numerator, Denominator, Metric, Score, Flag)
 
-  SRS_auto <- analyzed %>% CalculateRiskScore()
-
+  SRS_auto <- analyzed[[13]]$Analysis_Summary
   expect_equal(SRS_by_hand, SRS_auto)
 })
